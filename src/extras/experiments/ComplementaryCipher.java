a1651f4923daabcada30c9918779cf9a201fae9f
Burak Şahin <suspicioususer>
2017-02-27 16:13:45 +0300

    Revert "Revert "Some experiments.""
    This reverts commit 42901e954c6f2a974d7fc25d8a408df442d63a15.

--- src/extras/experiments/ComplementaryCipher.java
+++ src/extras/experiments/ComplementaryCipher.java
@@ -1,73 +1,59 @@
-package assignments.chapter2;
+package extras.experiments;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-public class ShiftCipherHM {
+public class ComplementaryCipher {
 
-	public static String Encryptor(String plainText, int shiftCount, HashMap<Character, Integer> hashMap,
+	public static String Encryptor(String plainText, HashMap<Character, Integer> hashMap,
 			HashMap<Integer, Character> inverseHashMap) {
 		char plainTextArray[] = plainText.toLowerCase().toCharArray();
-		int temp = shiftCount % 26;
 		for (int i = 0; i < plainText.length(); i++) {
 			int a = hashMap.get(plainText.charAt(i));
-			a = (a + temp) % 26;
+			a = 25 - a;
 			plainTextArray[i] = inverseHashMap.get(a);
 		}
 		return new String(plainTextArray);
 	}
 
-	public static String Decryptor(String encrypted, int shiftCount, HashMap<Character, Integer> hashMap,
+	public static String Decryptor(String encrypted, HashMap<Character, Integer> hashMap,
 			HashMap<Integer, Character> inverseHashMap) {
 		char encryptedArray[] = encrypted.toLowerCase().toCharArray();
-		int temp = shiftCount % 26;
 		for (int i = 0; i < encrypted.length(); i++) {
 			int a = hashMap.get(encrypted.charAt(i));
-			a = a - temp;
-			if(a < 0 ){
-				int x;
-				x = Math.abs(a) % 26;
-				a = 26 - x;
-			}else{
-				a = a % 26;
-			}
+			a = 25 - a;
 			encryptedArray[i] = inverseHashMap.get(a);
 		}
 		return new String(encryptedArray);
 	}
 
 	public static void main(String[] args) throws IOException {
-		int shiftCount = 0;
 		String plainText = null, encrypted = null, decrypted = null;
 		HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
 		HashMap<Integer, Character> inverseHashMap = new HashMap<Integer, Character>();
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 		for (int i = 0; i < 26; i++) {
-			hashMap.put((char) (i + 97), i); // fill the hashmap with z26
-												// (english alphabet) using
-												// ASCII codes
-			inverseHashMap.put(i, (char) (i + 97)); // inverse of alphabet
+			hashMap.put((char) (i + 97), i);
+			inverseHashMap.put(i, (char) (i + 97));
 		}
 
-		System.out.println("For exit enter negative value to shift count.");
 		while (true) {
-			System.out.println("Shift Count : ");
-			shiftCount = Integer.parseInt(bufferedReader.readLine());
-			if (shiftCount < 0) {
+			System.out.println("Plain Text : ");
+			plainText = bufferedReader.readLine();
+			if (plainText.equals("") || plainText.equals(null)) {
 				System.out.println("Program terminated.");
 				break;
 			}
-			System.out.println("Plain Text : ");
-			plainText = bufferedReader.readLine();
 			System.out.println("Original Data : " + plainText);
-			encrypted = Encryptor(plainText, shiftCount, hashMap, inverseHashMap);
+			encrypted = Encryptor(plainText, hashMap, inverseHashMap);
 			System.out.println("Encrypted Data : " + encrypted);
-			decrypted = Decryptor(encrypted, shiftCount, hashMap, inverseHashMap);
+			decrypted = Decryptor(encrypted, hashMap, inverseHashMap);
 			System.out.println("Decrypted Data : " + decrypted);
 		}
 		bufferedReader.close();
 	}
+
 }
